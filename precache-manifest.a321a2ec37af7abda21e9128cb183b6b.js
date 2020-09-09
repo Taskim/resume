4666b4a958f9f380bb3ac0181eda99d38ee650fc
Taskim <miksa_t@etna-alternance.net>
2020-09-09 22:52:42 +0200

    Updates

---
+++ precache-manifest.a321a2ec37af7abda21e9128cb183b6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad4ef3c142185648c71dc32f98d6d3e",
+    "url": "/scroll-snap-resume/index.html"
+  },
+  {
+    "revision": "9ab500b148a4b58876d6",
+    "url": "/scroll-snap-resume/static/css/main.4e5acb60.chunk.css"
+  },
+  {
+    "revision": "8c8736575dd7346888df",
+    "url": "/scroll-snap-resume/static/js/2.63e8e329.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/scroll-snap-resume/static/js/2.63e8e329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab500b148a4b58876d6",
+    "url": "/scroll-snap-resume/static/js/main.d88ab831.chunk.js"
+  },
+  {
+    "revision": "90342e9611ab6b635c1a",
+    "url": "/scroll-snap-resume/static/js/runtime-main.ba37ace6.js"
+  },
+  {
+    "revision": "bb5b12d2e8f0516400363ad9475cc68d",
+    "url": "/scroll-snap-resume/static/media/evaneos.bb5b12d2.svg"
+  },
+  {
+    "revision": "3368e2e77d3c048c0d8617f091e0be32",
+    "url": "/scroll-snap-resume/static/media/hairfie.3368e2e7.svg"
+  },
+  {
+    "revision": "f35a934f29c024f3dba6c39cc3baad4c",
+    "url": "/scroll-snap-resume/static/media/pp.f35a934f.png"
+  },
+  {
+    "revision": "09c6f32d1b877eede84ec350fc68fd3a",
+    "url": "/scroll-snap-resume/static/media/student.09c6f32d.svg"
+  }
+]);
